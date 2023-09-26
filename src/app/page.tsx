1d0a9b928c7d3f4047db1990b1fc608bfbe4b714
furium <shouldBnewB@gmail.com>
2023-09-26 17:52:06 +0900

    (Refactor) Navigation    [FRONTEND-2]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
 import { palette } from '@styles/theme';
 import Link from 'next/link';
+import NavigationDefault from '@component/navigation/NavigationDefault';
 
 const Container = styled.main`
   width: 100vw;
@@ -12,71 +13,11 @@ const Container = styled.main`
   color: ${palette.LINE};
   font-family: Montserrat, sans-serif;
 
-  nav {
-    z-index: 50;
-    width: 6%;
-    height: 100vh;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 5%;
-    padding-bottom: 30px;
-    display: flex;
-    position: fixed;
-
-    .branding__nav-menu {
-      flex: 0 auto;
-      margin-bottom: 40px;
-      transform: rotate(-90deg);
-      text-align: center;
-      letter-spacing: 5px;
-      text-transform: uppercase;
-      font-family: Montserrat, sans-serif;
-      font-size: 10px;
-      font-weight: 500;
-      transition: opacity 0.3s;
-      display: block;
-
-      &:hover {
-        text-decoration: line-through;
-      }
-    }
-
-    ul {
-      display: flex;
-      flex-direction: column;
-      /* gap: 10px; */
-      align-items: center;
-
-      .branding__nav-line {
-        width: 1px;
-        height: 100px;
-        background-color: ${palette.LINE};
-        margin-top: 10px;
-        margin-bottom: 80px;
-      }
-    }
-
-    .branding__nav-label {
-      flex: 0 auto;
-      margin-bottom: 40px;
-      transform: rotate(-90deg);
-      text-align: center;
-      letter-spacing: 5px;
-      text-transform: uppercase;
-      font-family: Montserrat, sans-serif;
-      font-size: 10px;
-      font-weight: 500;
-      transition: opacity 0.3s;
-      display: block;
-    }
-  }
-
   .branding__section-wrapper {
     padding-left: 10%;
     display: flex;
 
-    @media (max-width: 890px) {
+    @media (max-width: 1024px) {
       flex-direction: column;
       height: fit-content;
     }
@@ -90,7 +31,7 @@ const Container = styled.main`
       display: flex;
       /* position: fixed; */
 
-      @media (max-width: 890px) {
+      @media (max-width: 1024px) {
         height: fit-content;
         width: 100%;
         padding-top: 30%;
@@ -154,7 +95,7 @@ const Container = styled.main`
       transition-duration: 1s;
       transition-property: opacity;
 
-      @media (max-width: 890px) {
+      @media (max-width: 1024px) {
         height: fit-content;
         width: 100%;
       }
@@ -176,7 +117,7 @@ const Container = styled.main`
         position: relative;
         letter-spacing: -10px;
 
-        @media (max-width: 890px) {
+        @media (max-width: 1024px) {
           font-size: 15vw;
         }
 
@@ -195,62 +136,8 @@ const Container = styled.main`
     }
   }
 
-  @media screen and (max-width: 450px) {
+  @media screen and (max-width: 600px) {
     padding: 0 2% 0 2%;
-    nav {
-      width: 100%;
-      height: auto;
-      flex-direction: row;
-      padding-top: 2%;
-      padding-bottom: 0;
-      padding-left: 0;
-      position: static;
-
-      ul {
-        flex-direction: row;
-        gap: 10px;
-        align-items: center;
-
-        .branding__nav-menu {
-          flex: 0 auto;
-          margin: 0;
-          transform: rotate(0deg);
-          text-align: center;
-          letter-spacing: 5px;
-          text-transform: uppercase;
-          font-family: Montserrat, sans-serif;
-          font-size: 10px;
-          font-weight: 500;
-          transition: opacity 0.3s;
-          display: block;
-
-          &:hover {
-            text-decoration: line-through;
-          }
-        }
-
-        .branding__nav-line {
-          width: 100px;
-          height: 1px;
-          background-color: ${palette.LINE};
-          margin: 0;
-        }
-      }
-
-      .branding__nav-label {
-        flex: 0 auto;
-        margin: 0;
-        transform: rotate(0deg);
-        text-align: center;
-        letter-spacing: 5px;
-        text-transform: uppercase;
-        font-family: Montserrat, sans-serif;
-        font-size: 10px;
-        font-weight: 500;
-        transition: opacity 0.3s;
-        display: block;
-      }
-    }
 
     .branding__section-right {
       margin-left: 0 !important;
@@ -276,22 +163,7 @@ const RootPage = () => {
   }, []);
   return (
     <Container>
-      <nav>
-        <ul>
-          <li className="branding__nav-menu">
-            <Link href="https://github.com/contability" target="_blank">
-              GIT
-            </Link>
-          </li>
-          <li className="branding__nav-menu">
-            <Link href="https://open.kakao.com/o/swjepgJf" target="_blank">
-              KKO
-            </Link>
-          </li>
-          <li className="branding__nav-line" />
-        </ul>
-        <p className="branding__nav-label">since 2020</p>
-      </nav>
+      <NavigationDefault />
       <div className="branding__section-wrapper">
         <section className="branding__section-left">
           <h3 className="branding__section-header">SHINHYUNGJUNG</h3>
@@ -302,15 +174,13 @@ const RootPage = () => {
           <p className="branding__section-description">I enjoy constantly learning and developing</p>
         </section>
         <section className="branding__section-right fade">
-          <Link href="/" className="branding__section-right-menu">
+          <Link href="/services/work" className="branding__section-right-menu">
             work
           </Link>
           <Link href="/" className="branding__section-right-menu">
             about
           </Link>
-          <Link href="/" className="branding__section-right-menu">
-            shop
-          </Link>
+          <Link href="/" className="branding__section-right-menu"></Link>
           <Link href="/" className="branding__section-right-menu">
             contact
           </Link>
---
+++ src/app/services/work/page.tsx
@@ -0,0 +1,5 @@
+const workPage = () => {
+  return <div></div>;
+};
+
+export default workPage;
