4b94b2d3b63ec0425f1377c16a7fca7f6da20c1d
furium <shouldBnewB@gmail.com>
2023-10-10 16:27:26 +0900

    (add) profile image    [FRONTEND-4]

--- src/app/services/about/layout.tsx
+++ src/app/services/about/layout.tsx
@@ -3,11 +3,9 @@ import { palette } from '@styles/theme';
 
 export default function ServiceLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className=" scrollbar-hide">
-      <body>
-        <NavigationHome theme="warm" />
-        {children}
-      </body>
-    </html>
+    <>
+      <NavigationHome theme="warm" />
+      {children}
+    </>
   );
 }
--- src/app/services/about/page.tsx
+++ src/app/services/about/page.tsx
@@ -4,6 +4,7 @@ import Card from '@component/layout/Card';
 import { palette } from '@styles/theme';
 import styled from 'styled-components';
 import { aboutMe } from '../../../public/about';
+import Img from '@component/image/Img';
 
 const AboutMePageContainer = styled.article`
   width: 100vw;
@@ -14,6 +15,7 @@ const AboutMePageContainer = styled.article`
     padding-left: 10%;
     padding-right: 15%;
     display: flex;
+    position: relative;
 
     @media (max-width: 1024px) {
       flex-direction: column;
@@ -26,6 +28,7 @@ const AboutMePageContainer = styled.article`
 
     .about__section-contents {
       padding-top: 30%;
+      z-index: 10;
 
       .about__section-contents__header {
         font-size: 15vw;
@@ -47,6 +50,25 @@ const AboutMePageContainer = styled.article`
         gap: 5vw;
       }
     }
+
+    .about__section-contents__profile {
+      position: absolute;
+      width: 50%;
+      top: 15%;
+      right: 5%;
+
+      @media (max-width: 1024px) {
+        width: 75%;
+        top: 3%;
+        right: 0;
+      }
+
+      @media (max-width: 600px) {
+        width: 100%;
+        top: 5%;
+        right: -15%;
+      }
+    }
   }
 
   @media (max-width: 600px) {
@@ -58,8 +80,19 @@ const AboutMePage = () => {
   return (
     <AboutMePageContainer>
       <div className="about__section-wrapper">
+        <figure className="about__section-contents__profile">
+          <Img
+            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/images/image_furium.jpg`}
+            width={'100%'}
+            objectFit="contain"
+            aspect={'16/9'}
+          />
+        </figure>
         <section className="about__section-contents">
-          <h2 className="about__section-contents__header">About Me</h2>
+          <header className="about__section-contents__header">
+            <h2>About Me</h2>
+          </header>
+
           <div className="about__section-contents__main">
             {aboutMe.map((item, itemIndex) => (
               <Card key={`introduce-${itemIndex}`} title={item.title} content={item.content} />
